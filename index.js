036152c55a139f3a16e566c57ea6d7d12909828a
GorYeritsyan <gor.k.yeritsyan@gmail.com>
2026-03-10 18:55:03 +0400

    Modify strings

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const cardsWrapper = document.getElementById('cards-wrapper');
+const cardsWrapper = document.getElementById("cards-wrapper");
 
-const closeBtn = document.getElementById('close-btn');
-const playBtn = document.getElementById('play-btn');
-const modal = document.getElementById('modal');
-const header = document.getElementById('header');
+const closeBtn = document.getElementById("close-btn");
+const playBtn = document.getElementById("play-btn");
+const modal = document.getElementById("modal");
+const header = document.getElementById("header");
 
-const selectForm = document.getElementById('select-form');
+const selectForm = document.getElementById("select-form");
 
 // All Game Modes
 const gameModes = {
-    'easy': {
+    "easy": {
         columns: 3,
         rows: 4,
     },
@@ -34,11 +34,11 @@ const images = [
         url: "iphone.jpg"
     },
     {
-        id: 'airpods',
+        id: "airpods",
         url: "airpods.avif"
     },
     {
-        id: 'macbook',
+        id: "macbook",
         url: "macbook.webp"
     },
     {
@@ -46,11 +46,11 @@ const images = [
         url: "rome.png"
     },
     {
-        id: 'mango',
-        url: 'mango.png'
+        id: "mango",
+        url: "mango.png"
     },
     {
-        id: 'headphone',
+        id: "headphone",
         url: "headphones.webp"
     },
     {
@@ -58,28 +58,28 @@ const images = [
         url: "gaming-mouse.png"
     },
     {
-        id: 'boat',
+        id: "boat",
         url: "boat.webp"
     },
     {
-        id: 'home',
-        url: 'home.png'
+        id: "home",
+        url: "home.png"
     },
     {
         id: "soccer-ball",
         url: "soccer-ball.webp"
     },
     {
-        id: 'banana',
+        id: "banana",
         url: "banana.png"
     },
     {
-        id: 'clock',
+        id: "clock",
         url: "clock.png"
     },
     {
-        id: 'watermelon',
-        url: 'watermelon.png'
+        id: "watermelon",
+        url: "watermelon.png"
     },
     {
         id: "blueberry",
@@ -94,7 +94,7 @@ let isLocked = false;
 
 // Render Select tag from Game Modes
 window.addEventListener("DOMContentLoaded", () => {
-    const select = document.getElementById('select');
+    const select = document.getElementById("select");
 
     Object.keys(gameModes).forEach((key) => {
         const option = document.createElement("option");
@@ -105,7 +105,7 @@ window.addEventListener("DOMContentLoaded", () => {
 });
 
 // Add event listener to handle select form submit
-selectForm.addEventListener('submit', handleSelectFormSubmit);
+selectForm.addEventListener("submit", handleSelectFormSubmit);
 
 // Select Form Submit Handler
 function handleSelectFormSubmit(e) {
@@ -113,7 +113,7 @@ function handleSelectFormSubmit(e) {
 
     // Get game mode from form data
     const formData = new FormData(e.target);
-    const mode = formData.get('mode');
+    const mode = formData.get("mode");
 
     const gameColumns = gameModes[mode]?.columns;
     const gameRows = gameModes[mode]?.rows;
@@ -129,17 +129,17 @@ function handleSelectFormSubmit(e) {
 
     // Add Reset button to reset game
     const resetButton = document.createElement("button");
-    resetButton.id = 'reset-btn';
-    resetButton.className = 'px-3 py-2 bg-blue-600 hover:bg-blue-700 text-white cursor-pointer font-semibold rounded-sm w-fit self-center';
-    resetButton.textContent = 'Reset Game';
-    resetButton.addEventListener('click', resetGame);
+    resetButton.id = "reset-btn";
+    resetButton.className = "px-3 py-2 bg-blue-600 hover:bg-blue-700 text-white cursor-pointer font-semibold rounded-sm w-fit self-center";
+    resetButton.textContent = "Reset Game";
+    resetButton.addEventListener("click", resetGame);
 
     header.appendChild(resetButton)
 }
 
 // Function to change grid columns and rows
 function changeCardsLayout(columns = 3, rows = 4) {
-    cardsWrapper.className = `grid gap-5`;
+    cardsWrapper.className = "grid gap-5";
 
     // Dynamic change grid columns and rows
     cardsWrapper.style.gridTemplateColumns = `repeat(${columns},minmax(140px,1fr))`;
@@ -167,25 +167,25 @@ function renderRandomImages(columns, rows) {
 function renderCard(cardId, imageUrl) {
     const cardWrapper = document.createElement("div");
     cardWrapper.dataset.id = cardId;
-    cardWrapper.className = 'bg-transparent perspective-midrange rounded-md w-full cursor-pointer';
-    cardWrapper.addEventListener('click', handleCardFlip)
+    cardWrapper.className = "bg-transparent perspective-midrange rounded-md w-full cursor-pointer";
+    cardWrapper.addEventListener("click", handleCardFlip)
 
     const card = document.createElement("div");
-    card.className = 'relative w-full h-full rounded-md text-center transform-3d shadow shadow-[0_0_8px_2px_gray] rounded-xl transition-all duration-500';
+    card.className = "relative w-full h-full rounded-md text-center transform-3d shadow shadow-[0_0_8px_2px_gray] rounded-xl transition-all duration-500";
 
     const imageWrapper = document.createElement("div");
-    imageWrapper.className = 'absolute w-full h-full backface-hidden p-3 bg-white overflow-hidden rounded-xl rotate-y-180';
+    imageWrapper.className = "absolute w-full h-full backface-hidden p-3 bg-white overflow-hidden rounded-xl rotate-y-180";
 
     const image = document.createElement("img");
-    image.className = 'size-full object-contain';
+    image.className = "size-full object-contain";
     image.src = "./images/" + imageUrl;
 
     imageWrapper.appendChild(image);
 
     const backfaceWrapper = document.createElement("div");
-    backfaceWrapper.className = 'absolute w-full h-full flex justify-center items-center backface-hidden bg-green text-white overflow-hidden rounded-xl';
+    backfaceWrapper.className = "absolute w-full h-full flex justify-center items-center backface-hidden bg-green text-white overflow-hidden rounded-xl";
     const backfaceImage = document.createElement("img");
-    backfaceImage.className = 'h-full w-full object-contain';
+    backfaceImage.className = "h-full w-full object-contain";
     backfaceImage.src = "./images/card-backface.jpg";
 
     backfaceWrapper.appendChild(backfaceImage);
@@ -205,7 +205,7 @@ function handleCardFlip(e) {
     if (isLocked || card.classList.contains("rotate-y-180")) return;
 
     // Rotate card
-    card.classList.toggle('rotate-y-180');
+    card.classList.toggle("rotate-y-180");
 
     // If there is no first card
     if (!firstCard) {
@@ -220,8 +220,8 @@ function handleCardFlip(e) {
     // Rotate selected cards if first and second cards don't matching
     if(firstCard.dataset.id !== secondCard.dataset.id) {
         setTimeout(() => {
-            firstCard.firstElementChild.classList.remove('rotate-y-180');
-            secondCard.firstElementChild.classList.remove('rotate-y-180');
+            firstCard.firstElementChild.classList.remove("rotate-y-180");
+            secondCard.firstElementChild.classList.remove("rotate-y-180");
 
             reset();
         }, 500)
@@ -234,11 +234,11 @@ function handleCardFlip(e) {
     // Check if every card is already rotated or not
     // If rotated then show modal
     const isMatching = [...cardsWrapper.children].every(elem => {
-        return elem.firstElementChild.classList.contains('rotate-y-180')
+        return elem.firstElementChild.classList.contains("rotate-y-180");
     });
 
     if(isMatching) {
-        modal.classList.remove('hidden')
+        modal.classList.remove("hidden");
     }
 }
 
@@ -251,31 +251,31 @@ function reset() {
 
 // Function to close modal
 function closeModal() {
-    modal.classList.add('hidden');
+    modal.classList.add("hidden");
 }
 
 // Event listener to Close Modal when clicked the outside of modal
-modal.addEventListener('click', (e) => {
-    if(e.target.id === 'modal') {
-        closeModal()
+modal.addEventListener("click", (e) => {
+    if(e.target.id === "modal") {
+        closeModal();
     }
 });
 
 // Event listener to Close modal when clicked close button
-closeBtn.addEventListener('click', closeModal);
+closeBtn.addEventListener("click", closeModal);
 
 // Function to reset game
 function resetGame() {
-    const resetButton = document.getElementById('reset-btn');
+    const resetButton = document.getElementById("reset-btn");
     resetButton.remove();
-    cardsWrapper.innerHTML = '';
-    cardsWrapper.className = '';
+    cardsWrapper.innerHTML = "";
+    cardsWrapper.className = "";
 
     header.appendChild(selectForm);
 }
 
 // Reload page to play again
 playBtn.addEventListener("click", () => {
-    closeModal()
-    resetGame()
+    closeModal();
+    resetGame();
 });
